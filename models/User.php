b28700b1c3745cd2337acdcc87ce601858c806e8
Dimitry Krupenko <dmitry.krupenko@gmail.com>
2019-03-19 17:53:09 +0200

    Change auth

--- models/User.php
+++ models/User.php
@@ -107,7 +107,7 @@ class User extends ActiveRecord implements IdentityInterface
 	 */
 	public static function findIdentityByAccessToken($token, $type = null)
 	{
-		return static::findOne(['access_token' => $token]);
+		return static::findOne(['auth_key' => $token]);
 	}
 
 	/**
